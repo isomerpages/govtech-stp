669d4e3026029ec98402c93541700999d0cdee0e
gweiying <39231249+gweiying@users.noreply.github.com>
2021-05-18 11:18:53 +0800

    Migration (#753)
    * migrate: upgrading Jekyll to 4.0, changing gem dependencies
    
    * migrate: modifying collections, image, and file structure
    
    * migrate: add custom netlify.tml file

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+###############################################################
+# Generate build script for Jekyll v4 collections structure   #
+###############################################################
+# search within all collections for collection.yml file
+collections=$(find . -path ./_site -prune -false -o -name collection.yml -type f)
+var=$(echo $collections | sed 's/ .\//,.\//g')
+env='development'
+while getopts "e:" opt; do
+  case $opt in
+    e) env=$OPTARG      ;;
+    *) echo 'error' >&2
+       exit 1
+  esac
+done
+# netlify build
+JEKYLL_ENV=$env jekyll build --config _config.yml",$var"
